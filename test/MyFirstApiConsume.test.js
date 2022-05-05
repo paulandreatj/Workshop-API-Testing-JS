69d8abf76ae5c3866652a86d26dbeb7ed4e01910
Paula Taborda <ptaborda@unal.edu.co>
2022-05-04 21:16:52 -0500

    Schemes validation

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -25,17 +25,13 @@ describe('First Api Tests', () => {
     expect(response.body.args).to.eql(query);
   });
 
-  it('Consume DELETE Service with query parameters', async () => {
-    const query = {
-      name: 'John',
-      age: '31',
-      city: 'New York'
-    };
-
-    const response = await agent.delete('https://httpbin.org/delete').query(query);
+  it('Consume HEAD Service', async () => {
+    const response = await agent.head('https://httpbin.org/ip');
 
     expect(response.status).to.equal(statusCode.OK);
-    expect(response.body.args).to.eql(query);
+    expect(response.headers).to.have.property('content-length');
+    expect(response.headers).to.have.property('content-type');
+    expect(response.header['Content-Type'], /json/);
   });
 
   it('Consume PATCH Service with query parameters', async () => {
@@ -48,45 +44,41 @@ describe('First Api Tests', () => {
     const response = await agent.patch('https://httpbin.org/patch').query(query);
 
     expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.data).to.eq('');
     expect(response.body.args).to.eql(query);
   });
 
-  it('Consume PUT Service with query parameters', async () => {
-    const query = {
+  it('Consume PUT Service with sending Json', async () => {
+    const json = {
       name: 'John',
       age: '31',
       city: 'New York'
     };
+    const header = {
+      Accept: '*/*'
+    };
 
-    const response = await agent.put('https://httpbin.org/put').query(query);
+    const response = await agent.put('https://httpbin.org/put').send(json).set(header);
 
     expect(response.status).to.equal(statusCode.OK);
-    expect(response.body.args).to.eql(query);
+    expect(response.body.json).to.eql(json);
+    expect(response.body.headers).to.have.property('Accept');
   });
 
-  it('Consume PATCH Service with query parameters', async () => {
-    const query = {
+  it('Consume DELETE Service with sending Json', async () => {
+    const json = {
       name: 'John',
       age: '31',
       city: 'New York'
     };
-
-    const response = await agent.patch('https://httpbin.org/patch').query(query);
-
-    expect(response.status).to.equal(statusCode.OK);
-    expect(response.body.args).to.eql(query);
-  });
-
-  it('Consume HEAD Service with query parameters', async () => {
-    const query = {
-      name: 'John',
-      age: '31',
-      city: 'New York'
+    const header = {
+      Accept: '*/*'
     };
 
-    const response = await agent.head('https://httpbin.org/get').query(query);
+    const response = await agent.del('https://httpbin.org/delete').send(json).set(header);
 
     expect(response.status).to.equal(statusCode.OK);
-    expect(response.body.args).to.eql(undefined);
+    expect(response.body.json).to.eql(json);
+    expect(response.body.headers).to.have.property('Accept');
   });
 });
---
+++ test/schema/ListPublicEvents.schema.js
@@ -0,0 +1,69 @@
+const listPublicEventsSchema = {
+  type: 'object',
+  body: {
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        id: { type: 'string' },
+        type: {
+          type: 'string',
+          enum: ['PullRequestEvent', 'WatchEvent', 'DeleteEvent', 'PushEvent', 'CreateEvent']
+        },
+        actor: {
+          type: 'object',
+          properties: {
+            id: { type: 'number' },
+            login: { type: 'string' },
+            display_login: { type: 'string' },
+            gravatar_id: { type: 'string' },
+            url: { type: 'string' },
+            avatar_url: { type: 'string' }
+          }
+        },
+        repo: {
+          type: 'object',
+          properties: {
+            id: { type: 'number' },
+            name: { type: 'string' },
+            url: { type: 'string' }
+          }
+        },
+        payload: {
+          type: 'object',
+          properties: {
+            push_id: { type: 'number' },
+            size: { type: 'number' },
+            distinct_size: { type: 'number' },
+            ref: { type: ['null', 'string'] },
+            head: { type: 'string' },
+            before: { type: 'string' },
+            commits: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  sha: { type: 'string' },
+                  message: { type: 'string' },
+                  distinct: { type: 'boolean' },
+                  url: { type: 'string' },
+                  author: {
+                    type: 'object',
+                    properties: {
+                      email: { type: 'string' },
+                      name: { type: 'string' }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        public: { type: 'boolean' },
+        created_at: { type: 'string' }
+      }
+    }
+  }
+};
+
+exports.listPublicEventsSchema = listPublicEventsSchema;
